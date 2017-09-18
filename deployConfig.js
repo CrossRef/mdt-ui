9fd0159729552f92b0c94a43e3446ae5814e129b
leoandplus <lfabrikant@andplus.com>
2017-09-18 13:58:24 -0400

    presets for deployConfig

--- deployConfig.js
+++ deployConfig.js
@@ -1,6 +1,14 @@
 
-module.exports = {
-  baseUrl: '/metadatamanager/',
-  apiBaseUrl: 'http://mdt.crossref.org/mdt/v1'
+var presets = {
+  production: {
+    baseUrl: '/metadatamanager/',
+    apiBaseUrl: 'http://mdt.crossref.org/mdt/v1'
+  },
+
+  staging: {
+    baseUrl: '/mmstaging',
+    apiBaseUrl: 'https://apps.crossref.org/mdt-staging/'
+  }
 }
 
+module.exports = presets.production
