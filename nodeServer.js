f6210643f8e53ccb7525fa26324cc691a94b9581
leoandplus <lfabrikant@andplus.com>
2018-02-02 14:54:17 -0500

    removed vagrant server, added nodeServer

---
+++ nodeServer.js
@@ -0,0 +1,24 @@
+var deployConfig = require('./deployConfig')
+
+const express = require('express')
+
+const app = express();
+
+app.use(function logRequest (req, res, next) {
+  console.log(`${req.method}: ${req.originalUrl}`);
+  next()
+});
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/metadatamanager')
+
+
+app.use(deployConfig.baseUrl, express.static(__dirname + '/metadatamanager'));
+
+app.get('*', (req, res, next) => {
+  console.log('catchAll');
+  res.render('index.html');
+})
+
+app.listen(9005);
+console.log('Server listening on 9005')
