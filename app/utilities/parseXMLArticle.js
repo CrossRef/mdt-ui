2a6452b70230e8a6803263d1859d4286ccfffece
leoandplus <lfabrikant@andplus.com>
2018-02-05 14:58:46 -0500

    fixed parser

--- app/utilities/parseXMLArticle.js
+++ app/utilities/parseXMLArticle.js
@@ -257,9 +257,10 @@ const parseXMLArticle = function (articleXML) {
       references = Array.isArray(references.citation) ? references.citation : [references.citation]
 
       references.forEach( citation => {
+        const doi = typeof citation.doi === 'object' ? citation.doi['#text'] : citation.doi
         parsedReferences.push({
-          reference: citation.article_title || citation.unstructured_citation || 'Title not found in citation element',
-          DOI: typeof citation.doi === 'object' ? citation.doi['#text'] : citation.doi,
+          reference: citation.article_title || citation.unstructured_citation,
+          DOI: doi,
           matchValuation: citation.doi ? 61 : 30
         })
       })
