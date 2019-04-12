56d6fcd63c8adda164ebdec67825a75d99b09fe4
MikeYalter <myalter@crossref.org>
2019-04-12 11:52:04 -0400

    MM-335 rudiments of fundref resource XML generation
    Generation works, but doesn't create or deposit complete XML. Does not aggregate DOIs or award numbers. Does not validate DOIs or awards with no funders.

--- app/components/BulkUploadModal/resourcesXmlGenerator.js
+++ app/components/BulkUploadModal/resourcesXmlGenerator.js
@@ -1,4 +1,6 @@
 import csv from 'csvtojson'
+import {appendElm,appendAttribute, lowerCaseFirst} from '../../utilities/helpers'
+
 String.prototype.format = function () {
   var a = this;
   for (var k in arguments) {
@@ -43,18 +45,70 @@ DOI: "10.5555/nar/gkr1016"
 /*
 takes a parsed csv object and generates a deposit standard XML
 */
+const funderElems=['<funder_name>','<funder_identifier>','<award_number>']
 
-const f = function (headers,  files) {
-  console.log("yo")
+const f = function (headers, files) {
   console.log(header.format("this is the filename.java", "This is the email@.com"))
-  console.log(headers)
+
+  handleReadFiles(files, headers)
+
+}
+const cb = function (jsonArrayObj) {
+  console.log(jsonArrayObj)
+  var doc = new DOMParser().parseFromString('<doi_resources></doi_resources>','text/xml')
+  // iterate over the entire list of objects
+  var results = jsonArrayObj.map((row) => {
+    var xml
+    if (row['<funder_name>'])
+    {
+      xml=getFunderXml(row,doc)
+    }
+
+    return xml
+  })
+  results.filter((item)=>{return !!item}).reduce((doc,item)=>{
+    doc.documentElement.append(item)
+     return doc},doc)
   
-  console.log(handleReadFiles(files, headers))
-  console.log("done awaiting")
+  //console.log(doc)
+  console.log(new XMLSerializer().serializeToString(doc).replace(/></g,">\n<"))
 }
-const cb = function (jsonobj){
-  console.log(jsonobj)
+ 
+const elemReducer = function (row){
+  return function (funderElm,key){
+    if (!row[key]||row[key].trim().length<1){
+      return funderElm
+    }
+    var assertion = funderElm.ownerDocument.createElement("fr:assertion")
+    assertion.setAttribute("name", key.replace(/[<>]/g,""))
+    assertion.textContent = row[key].trim()
+    funderElm.appendChild(assertion)
+    //appendElm(,row[key],programElm)
+    return funderElm
+  }
 }
+function getFunderXml(row,doc)
+{
+  var funderElm = doc.createElement('fundref_data')
+  
+  var doiElm=doc.createElement('DOI')
+  doiElm.textContent=row.DOI.trim()
+  funderElm.appendChild(doiElm)
+  var programElm = doc.createElementNS("http://www.crossref.org/fundref.xsd", "fr:program")
+  programElm.setAttribute("name", "fundref")
+
+  funderElm.appendChild(
+    funderElems.filter((item)=>Object.keys(row).indexOf(item)>-1) // only process keys for funders
+      .reduce(elemReducer(row),programElm) ) // for all key/vals inrow accumulate result in programElm 
+  return funderElm
+}
+function getLicenseXml(row,doc)
+{
+  var funderElm = doc.createElement('fundref_data')
+  return funderElems.filter((item)=>Object.keys(row).indexOf(item)>-1).reduce(elemReducer(row),funderElm)  
+}
+
+
 function handleReadFiles(files, headers) {
   //oFiles = document.getElementById("uploadInput").files,
   //nFiles = oFiles.length;    
@@ -69,13 +123,12 @@ function handleReadFiles(files, headers) {
             headers: headers
           })
           .fromString(reader.result).then((jsonObj) => {
-          cb(jsonObj)
+            cb(jsonObj)
           })
       }
-      const res =  reader.readAsText(files[0])
+      const res = reader.readAsText(files[0])
       return res
-      //csv.fs.createReadStream(files[0])
-      
+      //csv.fs.createReadStream(files[0])      
     }
   }
 }
