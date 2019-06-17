d30ef88acd5a44d6fd7dc49144a17dc450810203
MikeYalter <myalter@crossref.org>
2019-06-17 15:04:32 -0400

    remove ISSN format validator dependancy and use our own
    to fix the issn not validating without a -

--- app/containers/addPublicationModal.js
+++ app/containers/addPublicationModal.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react'
 import is from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import validators from 'format-validator'
 import { XMLSerializer, DOMParser } from 'xmldom'
 import * as api from '../actions/api'
 import {getPublications} from '../actions/application'
 import { submitPublication } from '../actions/application'
-import {appendElm,appendAttribute} from '../utilities/helpers'
+import {appendElm,appendAttribute,isISSN} from '../utilities/helpers'
 import {isDOI, isURL, asyncCheckDupeDoi, xmldoc, errorHandler} from '../utilities/helpers'
 import LanguageSelector from '../utilities/lists/language'
 import { ArchiveLocations } from '../utilities/lists/archiveLocations'
@@ -209,9 +208,9 @@ export default class AddPublicationModal extends Component {
 
     criticalErrors.showTitleEmptyError = !this.state.title.length
 
-    criticalErrors.onlineISSNInvalidError = this.state.electISSN.length ? !validators.isISSN(this.state.electISSN): false
+    criticalErrors.onlineISSNInvalidError = this.state.electISSN.length ? !isISSN(this.state.electISSN): false
     criticalErrors.onlineDuplicateISSN = false
-    criticalErrors.printISSNInvalidError = this.state.printISSN.length ? !validators.isISSN(this.state.printISSN): false
+    criticalErrors.printISSNInvalidError = this.state.printISSN.length ? !isISSN(this.state.printISSN): false
     criticalErrors.printDuplicateISSN = false
 
     criticalErrors.showURLEmptyError = !this.state.url.length
