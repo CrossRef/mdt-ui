40ed09273d39532ef5f2205a1085e908da25a114
mochajh <jhanna@crossref.org>
2019-08-02 07:45:55 -0400

    MM-491: updated code changes to address additonal issues.

--- app/containers/addPublicationModal.js
+++ app/containers/addPublicationModal.js
@@ -469,7 +469,9 @@ export default class AddPublicationModal extends Component {
     const isEdit = this.state.mode === 'edit'
     const disabledInput = isEdit ? {disabled: true, className: 'disabledDoi'} : {}
     const toggleHidden = this.state.mode === 'add' || this.props.doi == undefined ? { visibility: 'hidden' } : {visibility: 'visible'}
-    const crossmark = this.props.crossmarkPrefixes ? this.state.mode === 'add' ? true : this.props.crossmarkPrefixes.indexOf(this.state.DOI.substring(0,7)) !== -1 : false
+    const doiPrefix = this.state.DOI.substring(0,this.state.DOI.indexOf('/'))
+    // Owner will only appear when doing a search. Might want to bring owner along for use here.
+    const crossmark = this.props.owner || this.props.crossmarkPrefixes ? this.props.crossmarkPrefixes.indexOf(doiPrefix) !== -1 : false
     const errors = this.state.errors
 
     return (
